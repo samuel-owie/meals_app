5ff4551e463c1efa83a0ee66bca1b60cb29bc91e
samuel-owie <owiesamuelehizogie@gmail.com>
2021-11-22 00:56:59 +0100

    Update

---
+++ lib/category_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+
+  final String title;
+  final Color color;
+
+  CategoryItem(this.title, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
