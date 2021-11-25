31b204a1eba521ef4096dce92ea5620674c3bb33
samuel-owie <owiesamuelehizogie@gmail.com>
2021-11-25 17:10:27 +0100

    Bottom tabs activated

--- lib/screens/tabs_screen_bottom.dart
+++ lib/screens/tabs_screen_bottom.dart
@@ -10,35 +10,46 @@ class TabsScreenB extends StatefulWidget {
 }
 
 class _TabsScreenBState extends State<TabsScreenB> {
+
+  final List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoritesScreen(),
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 1, // selects the default tab that should be preselected
-      child: Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text('Meals',),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: 
-                  Icon(Icons.category), 
-                  text: 'Categories',
-                ),
-              Tab(
-                icon: 
-                  Icon(Icons.star), 
-                  text: 'Favorites',
-              ),
-            ],
+        ),
+        body: _pages[_selectedPageIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Theme.of(context).accentColor,
+          currentIndex: _selectedPageIndex,
+          selectedFontSize: 14,
+          unselectedFontSize: 14,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text('Categories'),
             ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text('Favorites'),
+            ),
+          ]
         ),
-        body: TabBarView(children: <Widget>[
-          CategoriesScreen(),
-          FavoritesScreen(),
-        ]
-        ),
-      ),
-    );
+      );
   }
 }
